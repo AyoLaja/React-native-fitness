eb4b03812972ed419a058e11e6268b0a594aadf4
Ayomide Onalaja <ayo.onalaja@gmail.com>
2019-05-12 05:38:42 -0400

    completed date component

--- App.js
+++ App.js
@@ -1,29 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import AddEntry from './components/AddEntry';
 
 export default class App extends React.Component {
-  componentDidMount() {
-    console.log('before')
-    debugger;
-    console.log('after')
-  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Opening up App.js to start working on your app! Chee</Text>
+        <AddEntry />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#1a1a1a',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  text: {
-    color: '#fff'
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#1a1a1a'
   }
-});
+})
---
+++ components/UdacityStepper.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function UdacityStepper() {
+    return (
+        <View>
+            <Text>Udacity Stepper</Text>
+        </View>
+    )
+}
